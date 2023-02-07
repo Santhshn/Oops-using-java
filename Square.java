f6d70655da39d6b21c5a9171f0cde0245916ddf8
SANTHOSH.N <85983156+Santhshn@users.noreply.github.com>
2023-02-07 10:28:04 +0530

    Find Area of square in java

---
+++ Square.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Square
+{
+public static void main(String args[])
+{
+
+Scanner s = new Scanner(System.in);
+System.out.println("enter the area:");
+int x=s.nextInt();
+
+
+int square=x*x;
+System.out.println("the area of square:"+square);
+}
+}
+
