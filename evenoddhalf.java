fc9326c187f49c734d70762f574186d982f78b22
SANTHOSH.N <85983156+Santhshn@users.noreply.github.com>
2023-07-21 13:59:29 +0530

    JAVA

---
+++ evenoddhalf.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class evenoddhalf {
+    public static void main(String[] args) {
+        int i;
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(i=0;i<x;i++)
+        {
+            if(i%2==0)
+            {
+                
+                System.out.println();
+            }
+        }
+    
+}
+}
